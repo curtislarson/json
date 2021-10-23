f8d353bae1c4c7dfff7bc427d482b661383954f3
Curtis Larson <quackware@gmail.com>
2021-10-23 14:16:48 -0400

    Use deno test instead

---
+++ test.ts
@@ -0,0 +1,18 @@
+import Ajv from "https://esm.sh/ajv";
+import { expandGlob } from "https://deno.land/std@0.112.0/fs/mod.ts";
+import { red } from "https://deno.land/std@0.112.0/fmt/colors.ts";
+
+const ajv = new Ajv();
+for await (const file of expandGlob("./s/**/*")) {
+  Deno.test(`${file.path} is valid schema`, async () => {
+    const str = await Deno.readTextFile(file.path);
+    const schema = JSON.parse(str);
+    try {
+      ajv.addSchema(schema);
+      ajv.compile(schema);
+    } catch (e) {
+      console.error(red(`Validation failed for schema ${file.path}`), { e });
+      throw e;
+    }
+  });
+}
