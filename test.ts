6941e5a8efc39f44f880c2318be8473face7ef20
Curtis Larson <quackware@gmail.com>
2021-10-25 21:45:51 -0400

    some reorg

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import { expandGlob } from "https://deno.land/std@0.112.0/fs/mod.ts";
 import { red } from "https://deno.land/std@0.112.0/fmt/colors.ts";
 
 const ajv = new Ajv();
-for await (const file of expandGlob("./store/**/*")) {
+for await (const file of expandGlob("./dist/**/*.schema.json")) {
   Deno.test(`${file.path} is valid schema`, async () => {
     const str = await Deno.readTextFile(file.path);
     const schema = JSON.parse(str);
